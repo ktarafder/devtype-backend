6bf9f7105fe718fe090cbdd2920e189165bd0cbd
ktarafder <ktarafder01@gmail.com>
2024-10-30 22:52:13 -0400

    init commit

---
+++ src/test/java/com/example/devtype/DevtypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devtype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
